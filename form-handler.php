159d743e5b135f41cc544f8a13d1882ac050f263
mariia_f <EjAe6Z7kE69nFU5>
2023-03-27 00:02:39 +0200

    version 1 of the website

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'domain@name.com';
+$email_from = 'https://mrfedoren.github.io/index.html';
 
 $email_subject = 'New Form Submission';
 
